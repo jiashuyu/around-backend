d0195323a25d7dae00cffee811abb923f35a512d
Shuyu Jia <shuyuj2@illinois.edu>
2022-12-13 14:21:24 +0800

    My first commit

---
+++ constants/constants.go
@@ -0,0 +1,10 @@
+package constants
+
+const (
+	USER_INDEX  = "user"
+	POST_INDEX  = "post"
+	ES_URL      = "http://34.170.237.226:9200"
+	ES_USERNAME = "jiashuyu"
+	ES_PASSWORD = "Jiashuyu19960125!"
+	GCS_BUCKET  = "shuyu-around-bucket"
+)
